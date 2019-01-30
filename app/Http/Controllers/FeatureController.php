278bad582d432055ed68242854ffbe1941a721e2
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-01-30 11:47:46 +0700

    Removed unused things.

--- app/Http/Controllers/FeatureController.php
+++ app/Http/Controllers/FeatureController.php
@@ -14,21 +14,4 @@ class FeatureController extends Controller
 
         return view('feature.index', compact('features'));
     }
-
-    public function edit(Feature $feature)
-    {
-        return view('feature.edit', compact('feature'));
-    }
-    
-    public function update(Feature $feature)
-    {
-        $data = $this->validate(request(), [
-            'description' => 'required|string'
-        ]);
-
-        $feature->update($data);
-
-        return back()
-            ->with('message-success', __('messages.update.success'));
-    }
 }
