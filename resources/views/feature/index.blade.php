a25b2a2746a21ef2f156f230ab09df0e7d693a93
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-01-30 11:36:14 +0700

    Remove unused edit / create features.

--- resources/views/feature/index.blade.php
+++ resources/views/feature/index.blade.php
@@ -1,44 +1,38 @@
 @extends('shared.layout')
-@section('title', 'Kelola Fitur')
+@section('title', 'Daftar Fitur')
 @section('content')
 <div class="container my-5">
     <h1 class='mb-5'>
         <i class='fa fa-bar'></i>
-        Kelola Fitur
+        Daftar Fitur
     </h1>
 
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"> {{ config('app.name') }} </li>
-            <li class="breadcrumb-item active"> Kelola Fitur </li>
+            <li class="breadcrumb-item active"> Daftar Fitur </li>
         </ol>
     </nav>
 
     <div class="card">
         <div class="card-header">
             <i class="fa fa-bar"></i>
-            Kelola Fitur
+            Daftar Fitur
         </div>
         <div class="card-body">
             <div class='table-responsive'>
                 <table class='table table-sm table-striped'>
                    <thead class="thead thead-dark">
                         <tr>
-                            <th> # </th>
+                            <th> Kode </th>
                             <th> Deskripsi </th>
-                            <th> Aksi </th>
                         </tr>
                    </thead>
                    <tbody>
                        @foreach ($features as $feature)
                         <tr>
-                            <td> {{ $loop->iteration }}. </td>
+                            <td> F{{ $feature->id }} </td>
                             <td> {{ $feature->description }} </td>
-                            <td>
-                                <a href="{{ route('feature.edit', $feature) }}" class="btn btn-dark btn-sm">
-                                    <i class="fa fa-pencil"></i>
-                                </a>
-                            </td>
                         </tr>
                        @endforeach
                    </tbody>
