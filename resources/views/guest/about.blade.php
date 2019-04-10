25e9c458a4f6d29d01dae07c6a26b336170e9d7e
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-04-10 15:24:38 +0700

    About me page done.

--- resources/views/guest/about.blade.php
+++ resources/views/guest/about.blade.php
@@ -3,8 +3,30 @@
 @section('content')
 <div class="container my-5">
     <h1 class='mb-5'>
-        <i class='fa fa-icon'></i>
+        <i class='fa fa-info'></i>
         Tentang Saya
     </h1>
+
+    <div class="card">
+        <div class="card-body">
+           <dl>
+                <dt> Nama: </dt>
+                <dd> Dian Aulia Sari </dd>
+                
+                <dt> Tempat, Tanggal Lahir: </dt>
+                <dd> Singkawang, 14 Maret 1995 </dd>
+                
+                <dt> Riwayat Pendidikan </dt>
+                <dd> Teknik Informatika Universitas Tanjungpura </dd>
+
+                <dt> E-Mail: </dt>
+                <dd>
+                    <a href="mailto:dianiyank14@gmail.com">
+                        dianiyank14@gmail.com
+                    </a>
+                </dd>
+           </dl>
+        </div>
+    </div>
 </div>
 @endsection
