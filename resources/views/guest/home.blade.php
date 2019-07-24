1c2167272376a9189bb8c1630d1ade55acbf1147
Antonius Yonathan <atomicbomber.email@gmail.com>
2019-07-24 16:04:26 +0700

    Progress.

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -4,13 +4,13 @@
 
 <div style="background: black; height: 100%">
     <img
-        style="display: block; margin: auto"
+        style="display: block; height: 540px; width: auto; margin: auto"
         src="{{ asset("home.jpg") }}"
         alt="Gambar Ilustrasi Narkoba">
 </div>
 
 <div class="container my-5">
-    
+
 </div>
 
 @endsection
