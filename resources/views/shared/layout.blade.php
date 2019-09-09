2ac9bba0304d9d0ecd3fb8cd008d02f019f2bbd8
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-09-09 10:06:33 +0700

    wip.

--- resources/views/shared/layout.blade.php
+++ resources/views/shared/layout.blade.php
@@ -4,14 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> Sistem Informasi Diagnosis </title>
+    <title> {{ config('app.name') }} </title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/turbolinks.js') }}"></script>
 </head>
 <body>
     @include('shared.navbar')
-    @yield('content')
+
+    <div style="min-height: 80vh">
+        @yield('content')
+    </div>
+
+    <div class="bg-dark text-light">
+        <div class="container py-4 text-center">
+            © {{ now()->format('Y') }} - {{ config('app.name') }}
+        </div>
+    </div>
+
     <script src="{{ asset('js/app.js') }}"></script>]
     @yield('scripts')
 </body>
